951c891430ab6e8cae2b767e887fced2f0153cf7
ZakariyeAden <zakariye5417@gmail.com>
2022-12-24 11:42:03 -0600

    database

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,15 @@
 const sql = require('mysql')
-
-const db = sql.createConnection({
+const mysql = require('mysql2');
+// const db = sql.createConnection({
+//   host:"localhost",
+//   user:"root",
+//   password:"Boss2018!",
+//   database:"BlogPost",
+// })
+const db = mysql.createConnection({
   host:"localhost",
   user:"root",
   password:"Boss2018!",
-  database:"BlogPost",
+  database:"BlogPost"
 })
-
 module.exports = db
