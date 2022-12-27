26fcff914174e5cecc51801ab9d96bf2d82efb6a
ZakariyeAden <zakariye5417@gmail.com>
2022-12-27 13:52:02 -0600

    adding 3002 listen

--- server/index.js
+++ server/index.js
@@ -1,44 +1,55 @@
 const express = require("express");
 const app = express();
 const db = require("./config/db");
-const mysql = require("mysql2");
-const PORT = 3001;
-const cors = require('cors');
-// const connection = mysql.createConnection({
-//   host:"localhost",
-//   user:"root",
-//   password:"Boss2018!",
-//   database:"BlogPost"
-// })
-// connection.connect();
-// connection.connect(function(err) {
-//   if(err)
-//     throw err;
-//     console.log("connect");
-//     const sql = "INSERT INTO posts (title, post_text, user_name) VALUES('a title', 'asasassa', 'Zakariye')";
-//     connection.query(sql,function(err, result){
-//       if(err)
-//       throw err;
-//       console.log("Success");
-//     });
-// })
+
+const cors = require('cors')
+
+
 app.use(cors());
 app.use(express.json());
+
 // app.get("/", (req, res) => {
-db.query(
-  "INSERT INTO posts (title, post_text, user_name) VALUES('a title', 'newtest', 'Zakariye')"
-);
-// });
-// app.listen(PORT, () => {
-//   console.log(`Server running on Port ${PORT}`);
-// });
+//   console.log("red");
+  
+// db.query(
+//   "INSERT INTO posts (title, post_text, user_name) VALUES('a title', 'newtest', 'Zakariye')"
+// );
+// })
+
+
+app.get("/api/get", ( req,res) => {
+
+  db.query(
+    "SELECT * FROM posts",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+
+})
+
+// app.get("/api/getFromId/:id", ( req,res) => {
+// const id = req.params.id
+//   db.query(
+//     "SELECT * FROM posts WHERE id = ?", id, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       res.send(result);
+//     }
+//   );
+
+// })
 
 app.post("/api/create", (req, res) => {
   // Have to parse the data first in order to worl
-    const username = req.body.userName;
-    const title = req.body.title;
-    const text = req.body.text;
-  // console.log(username + title + text);
+  const username = req.body.userName;
+  const title = req.body.title;
+  const text = req.body.text;
+ 
 
   db.query(
     "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title,text,username],
@@ -50,3 +61,7 @@ app.post("/api/create", (req, res) => {
     }
   );
 });
+
+app.listen(3002, () => {
+  console.log('Server listening on port 3002')
+})
