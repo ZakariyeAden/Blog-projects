dab1ee9457cba8f4123c20b68cd4a44e6b7ab715
ZakariyeAden <zakariye5417@gmail.com>
2022-12-27 13:48:02 -0600

    adding substring for text

--- client/src/Components/Pages/MainPage.js
+++ client/src/Components/Pages/MainPage.js
@@ -1,14 +1,31 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
 function MainPage() {
+  const [postList, setPostList] = useState([]);
+  let naviagate = useNavigate();
+
+  useEffect(() => {
+    Axios.get("http://localhost:3002/api/get").then(data => {
+      setPostList(data.data);
+      console.log(data.data);
+    });
+  }, []);
+
   return (
     <div className="MainPage">
       <div className="PostContainer">
-        <div className="post">
-          <h1>Title</h1>
-          <p>Description</p>
-          <h4>Username</h4>
-        </div>
+        {postList.map((val, key) => {
+          return (
+            <div className="Post" key={key}>
+              <h1>{val.title}</h1>
+              <p>
+                {val.post_text.length > 500 ? val.post_text.substring(0,500) + "..." : val.post_text }
+              </p>
+              <h4>{val.user_name}</h4>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
