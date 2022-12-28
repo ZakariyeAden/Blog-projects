6c287a929c803b64299992a1b2d814fdbb1ae54c
ZakariyeAden <zakariye5417@gmail.com>
2022-12-28 13:18:10 -0600

    post page

--- client/src/Components/Pages/MainPage.js
+++ client/src/Components/Pages/MainPage.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
+
 function MainPage() {
   const [postList, setPostList] = useState([]);
   let naviagate = useNavigate();
 
   useEffect(() => {
-    Axios.get("http://localhost:3002/api/get").then(data => {
-      setPostList(data.data);
-      console.log(data.data);
-    });
+    Axios.get("http://localhost:3002/api/get")
+      .then(data => {
+        setPostList(data.data);
+        console.log(data.data);
+      })
+      .then(error => {
+        console.log(error);
+      });
   }, []);
 
   return (
@@ -17,10 +22,18 @@ function MainPage() {
       <div className="PostContainer">
         {postList.map((val, key) => {
           return (
-            <div className="Post" key={key}>
+            <div
+              className="Post"
+              key={key}
+              onClick={() => {
+                naviagate(`/post/${val.id}`);
+              }}
+            >
               <h1>{val.title}</h1>
               <p>
-                {val.post_text.length > 500 ? val.post_text.substring(0,500) + "..." : val.post_text }
+                {val.post_text.length > 500
+                  ? val.post_text.substring(0, 500) + "..."
+                  : val.post_text}
               </p>
               <h4>{val.user_name}</h4>
             </div>
