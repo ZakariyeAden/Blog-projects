e96c85cee37c33a06be36d3585146c4251010293
ZakariyeAden <zakariye5417@gmail.com>
2022-12-28 13:19:00 -0600

    single page

--- client/src/Components/Pages/Post.js
+++ client/src/Components/Pages/Post.js
@@ -1,27 +1,31 @@
-import React,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
+
 function Post() {
+  let { postId } = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    Axios.get(`http://localhost:3002/api/getFromId/${postId}`).then(data => {
+      console.log(data.data.post_text);
+      setPost({
+        title: data.data[0].title,
+        postText: data.data[0].post_text,
+        userName: data.data[0].user_name,
+        
+      });
+    });
+  }, []);
 
-  let {postId} = useParams();
-
-  // useEffect(() => {
-  //   Axios.get()
-  
-  
-  // }, [])
-  
   return (
     <div>
-      {/* <div className="post" > */}
-        {/* <h1>{val.title}</h1>
+      <div className="Post">
+        <h1>{post.title}</h1>
         <p>
-          {val.post_text.length > 500
-            ? val.post_text.subString(0, 500) + "..."
-            : val.post_text}
+          {post.postText}
         </p>
-        <h4>{val.user_name}</h4>
-      </div> */}
+        <h4>{post.userName}</h4>
+      </div>
     </div>
   );
 }
