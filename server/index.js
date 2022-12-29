7546abed39c68001d9f2a12508178e4e1a9798f4
ZakariyeAden <zakariye5417@gmail.com>
2022-12-29 15:19:14 -0600

    adding delete and allowing user to like

--- server/index.js
+++ server/index.js
@@ -2,25 +2,41 @@ const express = require("express");
 const app = express();
 const db = require("./config/db");
 
-const cors = require('cors')
-
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("works");
-  
-// db.query(
-//   "INSERT INTO posts (title, post_text, user_name) VALUES('a title', 'newtest', 'Zakariye')"
-// );
-})
 
+  // db.query(
+  //   "INSERT INTO posts (title, post_text, user_name) VALUES('a title', 'newtest', 'Zakariye')"
+  // );
+});
 
-app.get("/api/get", ( req,res) => {
-
+app.get("/api/get", (req, res) => {
+  db.query("SELECT * FROM posts", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+app.get("/api/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM posts WHERE id = ?",id, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+app.get("/api/like/:id", (req, res) => {
+  const id = req.params.id;
   db.query(
-    "SELECT * FROM posts",
+    "UPDATE posts SET likes = likes + 1 WHERE id = ?",
+    id,
     (err, result) => {
       if (err) {
         console.log(err);
@@ -28,31 +44,26 @@ app.get("/api/get", ( req,res) => {
       res.send(result);
     }
   );
-
-})
-
-app.get("/api/getFromId/:id", ( req,res) => {
-const id = req.params.id
-  db.query(
-    "SELECT * FROM posts WHERE id = ?", id, (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.send(result);
+});
+app.get("/api/getFromId/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM posts WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
     }
-  );
-
-})
+    res.send(result);
+  });
+});
 
 app.post("/api/create", (req, res) => {
   // Have to parse the data first in order to worl
   const username = req.body.userName;
   const title = req.body.title;
   const text = req.body.text;
- 
 
   db.query(
-    "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title,text,username],
+    "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",
+    [title, text, username],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -61,7 +72,7 @@ app.post("/api/create", (req, res) => {
     }
   );
 });
-// It has to be different then the localhost and from same the http 
+// It has to be different then the localhost and from same the http
 app.listen(3002, () => {
-  console.log('Server listening on port 3002')
-})
+  console.log("Server listening on port 3002");
+});
