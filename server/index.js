11e92a2ea088e743519d2a6a1ee7c5a8293a71cf
ZakariyeAden <zakariye5417@gmail.com>
2023-01-01 10:40:05 -0600

    adding parse

--- server/index.js
+++ server/index.js
@@ -25,31 +25,69 @@ app.get('/', (req, res) => {
     }
   )
 });
-
-app.get("/api/get", (req, res) => {
-  db.query("SELECT * FROM posts", (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(result);
-  });
-});
-app.get("/api/delete/:id", (req, res) => {
-    const id = req.params.id;
-    db.query("DELETE FROM posts WHERE id = ?",id, (err, result) => {
-      if (err) {
-        console.log(err);
+app.get('/api/get', (req, res) => {
+  request(
+    { url: 'https://zippy-valkyrie-5048e6.netlify.app/api/get' },
+    db.query("SELECT * FROM posts",(error, response, body, result) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
       }
-      res.send(result);
-    });
+      
+      res.json(JSON.parse(body));
+    })
+  )
 });
+// app.get("/api/get", (req, res) => {
+//   db.query("SELECT * FROM posts", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     res.send(result);
+//   });
+// });
 
-// app.put("/update", (req, res) => {
-//   const id = req.body.id;
-//   const title = req.body.title;
+app.get('/api/delete/:id', (req, res) => {
+  const id = req.params.id;
+  request(
+    { url: 'https://zippy-valkyrie-5048e6.netlify.app/api/delete/:id' },
+    db.query("DELETE FROM posts WHERE id = ?",id,(error, response, body, result) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      
+      res.json(JSON.parse(body));
+    })
+  )
+});
+// app.get("/api/delete/:id", (req, res) => {
+//     const id = req.params.id;
+//     db.query("DELETE FROM posts WHERE id = ?",id, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       res.send(result);
+//     });
+// });
+
+app.get('/api/like/:id', (req, res) => {
+  const id = req.params.id;
+  request(
+    { url: 'https://zippy-valkyrie-5048e6.netlify.app/api/like/:id' },
+    db.query("UPDATE posts SET likes = likes + 1 WHERE id = ?",id,(error, response, body, result) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      
+      res.json(JSON.parse(body));
+    })
+  )
+});
+// app.get("/api/like/:id", (req, res) => {
+
+//   const id = req.params.id;
 //   db.query(
-//     "UPDATE SET posts title = ? WHERE id = ?",
-//     [title,id],
+//     "UPDATE posts SET likes = likes + 1 WHERE id = ?",
+//     id,
 //     (err, result) => {
 //       if (err) {
 //         console.log(err);
@@ -58,51 +96,63 @@ app.get("/api/delete/:id", (req, res) => {
 //     }
 //   );
 // });
-app.get("/api/like/:id", (req, res) => {
 
+app.get('/api/getFromId/:id', (req, res) => {
   const id = req.params.id;
-  db.query(
-    "UPDATE posts SET likes = likes + 1 WHERE id = ?",
-    id,
-    (err, result) => {
-      if (err) {
-        console.log(err);
+  request(
+    { url: 'https://zippy-valkyrie-5048e6.netlify.app/api/getFromId/:id' },
+    db.query("SELECT * FROM posts WHERE id = ?",id,(error, response, body, result) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
       }
-      res.send(result);
-    }
-  );
+     
+      res.json(JSON.parse(body));
+    })
+  )
 });
+// app.get("/api/getFromId/:id", (req, res) => {
+//   const id = req.params.id;
 
-
-app.get("/api/getFromId/:id", (req, res) => {
-  const id = req.params.id;
-
-    db.query("SELECT * FROM posts WHERE id = ?", id, (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.send(result);
-    });
-  });
-
-app.post("/api/create", (req, res) => {
-
-    const username = req.body.userName;
+//     db.query("SELECT * FROM posts WHERE id = ?", id, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       res.send(result);
+//     });
+//   });
+app.get('/api/create', (req, res) => {
+  const username = req.body.userName;
     const title = req.body.title;
     const text = req.body.text;
-  
-    db.query(
-      "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",
-      [title, text, username],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log(result);
+  request(
+    { url: 'https://zippy-valkyrie-5048e6.netlify.app/api/create' },
+    db.query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title, text, username],(error, response, body, result) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
       }
-    );
-  // Have to parse the data first in order to worl
+      
+      res.json(JSON.parse(body));
+    })
+  )
 });
+// app.post("/api/create", (req, res) => {
+
+//     const username = req.body.userName;
+//     const title = req.body.title;
+//     const text = req.body.text;
+  
+//     db.query(
+//       "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",
+//       [title, text, username],
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         console.log(result);
+//       }
+//     );
+//   // Have to parse the data first in order to worl
+// });
 
 const PORT = process.env.PORT || 3002;
 // It has to be different then the localhost and from same the http
