6b8d38140cf5c7ee402aced6785d102eaa51bf5b
ZakariyeAden <zakariye5417@gmail.com>
2023-01-01 14:19:13 -0600

    _

--- server/config/db.js
+++ server/config/db.js
@@ -7,13 +7,13 @@ const db = mysql.createConnection({
   user:"bc9883e28ab581",
   password:"1dbc30b0",
   database:"heroku_08509d0775b4d6f",
+  ssl:{
+    rejectUnauthorized:false,
+  },
 })
 
 module.exports = db
 
 
-// host:"localhost",
-// user:"root",
-// password:"Boss2018!",
-// database:"BlogPost"
+
 
