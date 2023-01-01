b72f911320f74358e66b86b3f1f9163b45845b82
ZakariyeAden <zakariye5417@gmail.com>
2023-01-01 13:39:25 -0600

    _

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const cors = require("cors");
 app.use(express.json());
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin' ,'*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin' ,'*');
+//   next();
+// });
 
 
   db.query(
